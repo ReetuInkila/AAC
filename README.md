fbadc15301b376a2114cd5ca2561c3b7c7b3da90
Reetu Inkilä <inkilareetu@gmail.com>
2023-11-10 13:28:12 +0200

    settings xml

--- README.md
+++
@@ -1,2 +0,0 @@
-# AAC
-Android application for communicate with esp32 Wifi board
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Arduino controller"
+rootProject.name = "arduinoController"
 include(":app")
+ 
